f15ba55815f6149b8544498ed412ee14146778be
kwansaging <quitemarkfernan@gmail.com>
2024-03-27 13:12:12 +0800

    Script

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,13 @@ burger.addEventListener("click", () =>{
     burger.classList.toggle("open");
     headerA.classList.toggle("open");
 })
+
+const header = document.querySelector(".header");
+window.console.log(scrollY);
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    header.classList.add("dark");
+  } else {
+    header.classList.remove("dark");
+  }
+});
